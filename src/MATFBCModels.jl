05eb8919670780841fd47201232e711a96a2103c
St. Elmo Wilken <stelmozors@gmail.com>
2023-10-28 16:37:09 +0200

    rm deps, restructure

--- src/MATFBCModels.jl
+++ src/MATFBCModels.jl
@@ -2,21 +2,12 @@ module MATFBCModels
 
 using DocStringExtensions
 import AbstractFBCModels as A
-using MAT, SparseArrays, Reexport
+using MAT, SparseArrays
 
-module Internal
-using DocStringExtensions
-import ..A
 include("constants.jl")
-end
-
-import .Internal
-
-include("model.jl")
+include("interface.jl")
 include("io.jl")
 
 export MATFBCModel
-
-@reexport using AbstractFBCModels
 
 end # module MatFBCModels
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using Test
-import RequiredInterfaces as R
 import AbstractFBCModels as A
 import MATFBCModels as M
 
@@ -11,10 +10,8 @@ iml1515_path = A.download_data_file(
     "223db0b1ed69a7cc782f2a3093c1f48911a3c8bbd5bdf4bcdb13185cab5fdaa0",
 )
 
+iml1515_path = joinpath("test", "downloaded", "iML1515.mat")
+
 @testset "MATFBCModels" begin
-    @testset "Interface implemented" R.check_implementations(A.AbstractFBCModel)
-    # IO
-    include("io.jl")
-    # accessors
-    include("accessors.jl")
+
 end
