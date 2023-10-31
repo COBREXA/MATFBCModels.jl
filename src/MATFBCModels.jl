aaafeb19f3dbac63238210fe29db26c7d0c8694c
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2023-10-31 12:29:58 +0100

    import cleanup

--- src/MATFBCModels.jl
+++ src/MATFBCModels.jl
@@ -1,14 +1,17 @@
 module MATFBCModels
 
 using DocStringExtensions
+
 import AbstractFBCModels as A
-using MAT, SparseArrays
+
+using MAT
+using SparseArrays
 
 include("types.jl")
 include("constants.jl")
-include("utils.jl")
 include("interface.jl")
 include("io.jl")
+include("utils.jl")
 
 export MATFBCModel
 
